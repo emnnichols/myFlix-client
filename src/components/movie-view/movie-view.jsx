8e5e771de882144c344b11f584863f2c73155f2f
Emily N <emnnichols@outlook.com>
2024-02-06 20:56:22 -0800

    Created MovieView component

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,30 @@
+export const MovieView = ({ movie, onBackClick }) => {
+  return (
+    <div>
+      <div>
+        <img src={movie.poster} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Released: </span>
+        <span>{movie.release}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.genre}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+      <div>
+        <span>Summary: </span>
+        <span>{movie.summary}</span>
+      </div>
+      <button onClick={onBackClick}>Back</button>
+    </div>
+  )
+};
