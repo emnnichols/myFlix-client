784f4a015282c2dc6bcd81917a183e8805f0fc99
Emily N <emnnichols@outlook.com>
2024-02-06 22:17:14 -0800

    Built project with parcel

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,7 @@ import { MainView } from './components/main-view/main-view';
 import "./index.scss";
 
 const MyFlixApplication = () => {
-  return (
-    <MainView />
-  );
+  return <MainView />;
 };
 
 const container = document.querySelector("#root");
