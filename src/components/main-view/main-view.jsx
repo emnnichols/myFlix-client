8ecb4c3cc15f3819be9a918150abf26cde13ca7b
Emily N <emnnichols@outlook.com>
2024-02-16 14:55:53 -0800

    Added login and signup components to mainview

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,25 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
 
   useEffect(() => {
-    fetch("https://myflix-ghibli-7c8d5913b80b.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://myflix-ghibli-7c8d5913b80b.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
     .then((response) => response.json())
     .then((data) => {
       const moviesfromApi = data.map((movie) => {
@@ -26,7 +37,22 @@ export const MainView = () => {
 
       setMovies(moviesfromApi);
     });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return (
+    <>
+      <LoginView 
+        onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }}
+      />
+      or
+      <SignupView />
+    </>
+    );
+  }
 
   if (selectedMovie) {
     return (
@@ -40,6 +66,7 @@ if (movies.length === 0) {
 
 return (
   <div>
+    <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
     {movies.map((movie) => (
       <MovieCard 
       key={movie.id}
