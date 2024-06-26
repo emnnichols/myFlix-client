b745d43de02ba72d5ccc7ad6565f6fb02621b20c
Emily Nichols <emnnichols@outlook.com>
2024-06-26 00:28:09 -0700

    Reformat Director view

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -11,38 +11,38 @@ export const DirectorView = ({ about, movies, isFavorite, resetSearch, addFav, r
 
   return (
     <>
-    <Row className="justify-content-md-center">
-      <Link to="/">
-        <Button className="mt-3 w-100 primaryButton" variant="primary" onClick={resetSearch}>
-          Back
-        </Button>
-      </Link>
-        <br/>
-          <p className="paraText mt-4"><span className="h4">{director} | {birth
-            ? birth.replace(/["]+/g,'')
-            : birth} - {death 
-            ? death.replace(/["]+/g,'') 
+      <Row className="justify-content-md-center">
+        <Link to="/">
+          <Button className="mt-3 w-100 primaryButton" variant="primary" onClick={resetSearch}>
+            Back
+          </Button>
+        </Link>
+        <br />
+        <p className="paraText mt-4"><span className="h4">{director} | {birth
+          ? birth.replace(/["]+/g, '')
+          : birth} - {death
+            ? death.replace(/["]+/g, '')
             : `Present`}</span>
-        <br/>
-        <br/>
+          <br />
+          <br />
           {bio
-          ? bio.replace(/["]+/g,'')
-          : bio}</p>
-        <br/>
-        <br/>
+            ? bio.replace(/["]+/g, '')
+            : bio}</p>
+        <br />
+        <br />
         {movies.filter((movie) => {
           return movie.director.Name === director
         }).map((movie) => (
           <Col lg={3} md={6} className="mb-2">
-              <MovieCard 
-                movie={movie}
-                isFavorite={isFavorite}
-                removeFav={removeFav}
-                addFav={addFav}
-              />
+            <MovieCard
+              movie={movie}
+              isFavorite={isFavorite}
+              removeFav={removeFav}
+              addFav={addFav}
+            />
           </Col>
         ))}
-        </Row>
+      </Row>
     </>
   );
 };
